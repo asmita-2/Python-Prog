a4f327243ecfcc40d318965d1a61a96be50fb52e
asmita-2 <168653877+asmita-2@users.noreply.github.com>
2024-05-01 22:50:35 +0530

    Add files via upload

---
+++ batch_gradient.py
@@ -0,0 +1,42 @@
+###batch-gradient descent implementation
+import numpy as np
+import pandas as pd
+df = pd.read_csv("/home/ibab/simulated_data_multiple_linear_regression_for_ML.csv")
+
+insert = df.insert(0,'x_0',1)
+#print(df)
+
+n = df.shape[0]   #rows
+d = df.shape[1]   #cols
+#print(n,d)
+
+###division of data
+X = df.iloc[:, 0:d-2]
+y = df.iloc[:, d-2]
+#print(X, y)
+
+theta = np.zeros(6)
+h_x = 0
+###hypothesis function
+for i in range(0, d-2):
+   h_x += theta[i]*X.iloc[:, i]
+#print(h_x)
+
+cost_func = sum(0.5*(h_x - y)**2)
+print(cost_func)
+alpha = 0.0000001
+threshold = 0.0001
+a = 100000
+while a > threshold:
+   for j in range(d-2):
+      derivative = sum((h_x - y) * X.iloc[:, j])
+      theta[j] = theta[j] - alpha * derivative
+   h_x += theta[i] * X.iloc[:, i - 1]
+   cost_func_new = sum(0.5 * (h_x - y) ** 2)
+   print(cost_func_new)
+   a = cost_func - cost_func_new
+   if a < threshold:
+         break
+   cost_func = cost_func_new
+print(theta)
+
