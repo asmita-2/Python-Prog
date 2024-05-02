9a8542f1b94cead5f64b0c36aa90313f13bbc617
Asmita Mukherjee <168653877+asmita-2@users.noreply.github.com>
2024-05-02 18:58:38 +0530

    Update logistic_regression.py

--- logistic_regression.py
+++ logistic_regression.py
@@ -1,4 +1,5 @@
-###Implement logistic regression using scikit-learn for the breast cancer dataset and simulated dataset
+###This code shows the accuracy of logistic regression using scikit-learn for the breast cancer dataset and simulated dataset
+
 from sklearn.datasets import load_breast_cancer
 from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LogisticRegression
