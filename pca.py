090afe2deeb9ccf6d5f8e98592c2c50114430c63
Asmita Mukherjee <168653877+asmita-2@users.noreply.github.com>
2024-05-02 19:54:58 +0530

    Update pca.py

--- pca.py
+++ pca.py
@@ -1,4 +1,5 @@
-###PCA
+###this code performs pca is US-ARREST dataset.
+
 
 import pandas as pd
 import numpy as np
@@ -11,20 +12,28 @@ from sklearn.cluster import KMeans, AgglomerativeClustering
 from scipy.cluster.hierarchy import dendrogram, cut_tree
 from ISLP.cluster import compute_linkage
 
-
+###loading data.
 us_arrests = get_rdataset('USArrests').data
 #print(us_arrests)
 # print(us_arrests.columns)
 # print(us_arrests.mean())
 #print(us_arrests.var())
+
+###standardizing the data.
 scaler = StandardScaler(with_std=True, with_mean=True)
 us_scaled = scaler.fit_transform(us_arrests)
+
+##fitting the model
 pca_us = PCA()
 pca_us.fit(us_scaled)
 #print(pca_us.mean_)
 scores = pca_us.transform(us_scaled)
+
+##printing scores and components of pca.
 #print(scores)
 #print(pca_us.components_)
+
+### PCA visualisation bi-plot method
 i, j = 0, 1
 fig, ax = plt.subplots(1, 1, figsize=(8, 8))
 ax.scatter(scores[:, 0], scores[:, 1])
@@ -35,6 +44,8 @@ for k in range(pca_us.components_.shape[1]):
     ax.text(pca_us.components_[i, k],
             pca_us.components_[j, k],
             us_arrests.columns[k])
+
+##scaling
 scale_arrow = s_ = 2
 scores[:, 1] *= -1
 pca_us.components_[1] *= -1
@@ -48,9 +59,15 @@ for k in range(pca_us.components_.shape[1]):
             s_*pca_us.components_[j,k],
             us_arrests.columns[k])
 a = scores.std(0, ddof=1)
+
+# StdDev of Principal component scores
 #print(a)
+
+##Variance of each score
 b = pca_us.explained_variance_ratio_
 #print(b)
+
+##Plotting Proportion of variance explained
 fig, axes = plt.subplots(1, 2, figsize=(15, 6))
 ticks = np.arange(pca_us.n_components_)+1
 ax = axes[0]
@@ -59,6 +76,8 @@ ax.set_xlabel('principal component')
 ax.set_ylabel('proportion of variance explained')
 ax.set_ylim([0, 1])
 ax.set_xticks(ticks)
+
+###cumulative variance plot
 ax = axes[1]
 ax.plot(ticks,
 pca_us.explained_variance_ratio_.cumsum(), marker='o')
