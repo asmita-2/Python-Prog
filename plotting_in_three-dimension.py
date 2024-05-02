557b74d7f6801923098ce3b3ebb88c9eb8619638
Asmita Mukherjee <168653877+asmita-2@users.noreply.github.com>
2024-05-02 19:17:44 +0530

    Update plotting_in_three-dimension.py

--- plotting_in_three-dimension.py
+++ plotting_in_three-dimension.py
@@ -1,3 +1,7 @@
+###This code plots data points in three dimension space to make them look seperable by passing them through a tranform function.
+
+##importing libraries
+
 from matplotlib import pyplot as plt
 import numpy as np
 from sklearn import svm
@@ -7,14 +11,17 @@ import pandas as pd
 import warnings
 warnings.filterwarnings("ignore", category=DeprecationWarning)
 
-##Plot these points.  Then transform these points using your “Transform” function into 3-dim space. Plot the points and
+##ques) Plot the points. Then transform these points using a “Transform” function into 3-dim space. Plot the points and
 ## manipulate the points so that you can see a separating plane in 3D.
 
+##plotting  the points in 3-d space.
 def transform(X1, root, X2, label):
     fig = plt.figure()
     ax = fig.add_subplot(111, projection='3d')
     ax.scatter(X1, root, X2, c=label)
     plt.show()
+
+###converting the data into 3-d function.
 def threeD(x1,x2, label):
     X1 = []
     root = []
@@ -29,15 +36,20 @@ def threeD(x1,x2, label):
         cal3 = 2**0.5*x1[k]*x2[k]
         root.append(cal3)
     transform(X1, root, X2, label)
+
+###plotting a scatter plot of the data.
 def phi(x1,x2,label):
     plt.scatter(x1, x2, c=label)
     plt.show()
     threeD(x1, x2, label)
+
+###the given data.
 def main():
     x1 = [1, 1, 2, 3, 6, 9, 13, 18, 13, 18, 9, 6, 3, 2, 1, 1]
     x2 = [3, 6, 6, 9, 10, 11, 12, 16, 15, 6, 11, 5, 10, 5, 6, 3]
     label = ['Blue', 'Blue', 'Blue', 'Blue', 'Blue', 'Blue', 'Blue', 'Blue', 'Red', 'Red', 'Red', 'Red', 'Red', 'Red', 'Red', 'Red']
     phi(x1, x2, label)
+    
 if __name__=="__main__":
     main()
 
