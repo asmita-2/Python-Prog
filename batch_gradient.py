9af1b6698ae33f0c17633d14794f7af89bcdcf40
Asmita Mukherjee <168653877+asmita-2@users.noreply.github.com>
2024-05-02 17:09:05 +0530

    Update batch_gradient.py

--- batch_gradient.py
+++ batch_gradient.py
@@ -1,31 +1,40 @@
-### Implementing batch-gradient descent for the dataset simulated_data_multiple_linear_regression_for_ML.csv. This code shows how the cost function 
-### comes down and at convergenge prints all the parameters(theta values).
+### Implementing batch-gradient descent from scratch for the dataset simulated_data_multiple_linear_regression_for_ML.csv. This code shows how the cost
+### function comes down and at convergenge prints all the parameters(theta values).
 
+###importing necessary libraries
 import numpy as np
 import pandas as pd
+
+###loading the data
 df = pd.read_csv("/home/ibab/simulated_data_multiple_linear_regression_for_ML.csv")
 
+###inserting a column in the data containing value 1 in all the rows 
 insert = df.insert(0,'x_0',1)
 #print(df)
 
-n = df.shape[0]   #rows
-d = df.shape[1]   #cols
+
+n = df.shape[0]   #no. of rows
+d = df.shape[1]   #no. of cols
 #print(n,d)
 
-###division of data
+###division of data into features and target.
 X = df.iloc[:, 0:d-2]
 y = df.iloc[:, d-2]
 #print(X, y)
 
+##initializing parameter(theta)values
 theta = np.zeros(6)
 h_x = 0
-###hypothesis function
+
+### defining hypothesis function
 for i in range(0, d-2):
    h_x += theta[i]*X.iloc[:, i]
 #print(h_x)
 
+##definimg cost function and updating cost function
 cost_func = sum(0.5*(h_x - y)**2)
 print(cost_func)
+
 alpha = 0.0000001
 threshold = 0.0001
 a = 100000
@@ -40,5 +49,7 @@ while a > threshold:
    if a < threshold:
          break
    cost_func = cost_func_new
+   
+##Lastly printing the theta values
 print(theta)
 
